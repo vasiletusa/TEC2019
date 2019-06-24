ac6165b9b13114c98060e0d1b998f13d39e91a76
arbokcpp <albertosalviato@gmail.com>
2019-06-24 16:19:13 +0200

    update home

--- home.php
+++ home.php
@@ -1,21 +1,40 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+<link rel="stylesheet" href="stile.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <?php include('head.php') ?>
 
-<div id="riga1">
-		<ul>
-			<li><a href="Citta/Padova.html"><img src="img/padova.jpg" alt="foto della città di Padova"></a></li>
-			<li><a href="Citta/Venezia.html"><img src="img/venezia.jpg" alt="foto della città di Venezia"></a></li>
-			<li><a href="Citta/Rovigo.html"><img src="img/rovigo.jpg" alt="foto della città di Rovigo"></a></li>
-		</ul>	
-	</div>	
-	<div id="riga2">	
-		<ul>	
-			<li><a href="Citta/Belluno.html"><img src="img/belluno.jpg" alt="foto della città di Belluno"></a></li>
-			<li><a href="Citta/Vicenza.html"><img src="img/vicenza.jpg" alt="foto della città di Vicenza"></a></li>
-			<li><a href="Citta/Verona.html"><img src="img/verona.jpg" alt="foto della città di Verona"></a></li>
-			<li><a href="Citta/Treviso.html"><img src="img/treviso.jpg" alt="foto della città di Treviso"></a></li>
-		</ul>		
-	</div>
+<div class="container">
+<div class="gallery">
+	<a href="Citta/Padova.html"><img src="img/padova.jpg" alt="foto della città di Padova"></a>
+	<div class="desc">PADOVA<a href="Citta/Padova.html"></a></div>
+</div>			
+<div class="gallery">			
+	<a href="Citta/Venezia.html"><img src="img/venezia.jpg" alt="foto della città di Venezia"></a>
+	<div class="desc">VENEZIA<a href="Citta/Venezia.html"></a></div>
+</div>
+<div class="gallery">			
+	<a href="Citta/Rovigo.html"><img src="img/rovigo.jpg" alt="foto della città di Rovigo"></a>
+	<div class="desc">ROVIGO<a href="Citta/Rovigo.html"></a></div>
+</div>
+</div>
+<div class="container">	
+<div class="gallery">	
+	<a href="Citta/Belluno.html"><img src="img/belluno.jpg" alt="foto della città di Belluno"></a>
+	<div class="desc">BELLUNO<a href="Citta/Belluno.html"></a></div>
+</div>
+<div class="gallery">
+	<a href="Citta/Vicenza.html"><img src="img/vicenza.jpg" alt="foto della città di Vicenza"></a>
+	<div class="desc">VICENZA<a href="Citta/Vicenza.html"></a></div>
+</div>
+<div class="gallery">
+	<a href="Citta/Verona.html"><img src="img/verona.jpg" alt="foto della città di Verona"></a>
+	<div class="desc">VERONA<a href="Citta/Verona.html"></a></div>
+</div>
+<div class="gallery">			
+	<a href="Citta/Treviso.html"><img src="img/treviso.jpg" alt="foto della città di Treviso"></a>
+	<div class="desc">TREVISO<a href="Citta/Treviso.html"></a></div>
+</div>
+</div>
 
 <?php include('footer.php') ?>
