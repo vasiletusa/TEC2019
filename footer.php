905daa200f25d09b3bb0e542a3164a0865153629
arbokcpp <albertosalviato@gmail.com>
2019-07-09 17:56:04 +0200

    mod2

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer id="footer" class="footer">
 	<div class="container">		
-		<div>&copy; 2019 <span lang="en">Veneto-on-Tour</span> | Via Dante Alighieri 100 - 30035 Mirano (<span lang="en">Italy</span>)
+		<div>&copy; 2019 <span lang="en">Veneto-on-Tour</span> | Via Dante Alighieri 123 - 30035 Mirano (<span lang="en">Italy</span>)
 		</div>
 		<div> email: venetontour@gmail.com </div>
 		<div> tel: +39 04145678910 </div>
