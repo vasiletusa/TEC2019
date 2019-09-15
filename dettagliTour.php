9a759d3d3511243bcb6cdb1b34a89eb1b3555ebe
Your Name <you@example.com>
2019-09-15 18:22:32 +0200

    sistemazione dettour

--- dettagliTour.php
+++ dettagliTour.php
@@ -17,31 +17,27 @@
         $tour=tourDaId($_GET['id']);
         
         echo   "<div class=\"marginPrincipale\" >
-	<h1>".$tour['Titolo']."</h1> 
-	<div class=\"sinistra\">
-		<img src=\"img/padova.jpg\" alt=\"foto della città di Padova\" class=\"detourimg\" />
-		<p>DATA: <label for=\"data\">".$tour['Data']."</label></p>
-		<p>ORGANIZZATO DA :<label for=\"organiz\">".$tour['Organizzatore']."</label></p>
-		<p>CITTA' :<label for=\"citta\">".$tour['Citta']."</label> </p>
-		
-	</div>	
+					<h1>".$tour['Titolo']."</h1> 
+					<div class=\"sinistra\">
+						<img src=\"img/padova.jpg\" alt=\"foto della città di Padova\" class=\"detourimg\" />
+						<p class='coloret'>DATA: <label for=\"data\" class='coloret'>".$tour['Data']."</label></p>
+						<p class='coloret'>ORGANIZZATO DA :<label for=\"organiz\" class='coloret'>".$tour['Organizzatore']."</label></p>
+						<p class='coloret'>CITTA' :<label for=\"citta\" class='coloret'>".$tour['Citta']."</label> </p>
+						
+					</div>	
 
-	<div class=\"sfondo sinistra\">
-		<div id=\"descrTour\">  Descrizione del Tour </div>
-		<p>  <label for=\"Descrizione\" id=\"labelTour\">".$tour['Descrizione']."</label> </p>
-			
-	</div>
+					<div class=\"sfondo destra\">
+						<div id=\"descrTour\">  Descrizione del Tour </div>
+						<p>  <label for=\"Descrizione\" id=\"labelTour\">".$tour['Descrizione']."</label> </p>
+							
+					</div>
 
-"              
+					<div class='marginSecondario'> 
+						<input class='buttonIscrizione' name='iscrizione' type='submit' value='ISCRIVITI' />
+					</div>
+
+				</div>"              
     ?>
-
-
-
-<div class="marginSecondario"> 
-	<input class="buttonIscrizione" name="iscrizione" type="submit" value="ISCRIVITI" />
-</div>
-
-
 
 
 
