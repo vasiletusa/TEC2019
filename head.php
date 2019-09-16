e5f8e870f12fb002205d4eb4f40bfcbf7603ab14
arbokcpp <albertosalviato@gmail.com>
2019-09-16 15:12:41 +0200

    prc

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>TecWeb</title>
-	<link rel="stylesheet" href="style.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
 </head>
 
 <body>
