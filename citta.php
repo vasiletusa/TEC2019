7561b48785a5a0bedd34f80da4807d2ccd41f0ed
vasile <vasiletusa95@yahoo.it>
2019-09-16 14:32:13 +0200

    tabella citta

---
+++ citta.php
@@ -0,0 +1,27 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
+
+<?php include_once 'server.php'?>
+
+<head>
+	<?php 
+        include_once 'functions.php'; 
+	    getHead("Citta");    
+    ?>      
+
+</head>
+<?php getMenu("Info");?>
+<?php getBreadcumbs("citta");?>
+
+<div>
+  <h1> le citta del veneto</h1>
+  <p class="testo">  </p>
+</div>
+
+
+</body>
+
+<?php include('footer.php') ?>
+
+
+
+
---
+++ css/sql citta.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `città` (
+  `ID` int(11) NOT NULL,
+  `città` varchar(255) NOT NULL,
+  `descrizione` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
