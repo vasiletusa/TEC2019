c24e6388d0fa0637028fa9fd29a2e2725e27adf9
vasile <vasiletusa95@yahoo.it>
2019-09-16 20:01:46 +0200

    .

--- info.php
+++ info.php
@@ -14,17 +14,16 @@
 
 <div>
   <div><h1> Veneto Tour ti porta alla scoperta del Veneto</h1></div>
-  <div><h2>Offriamo un servizio pieno di emozioni con viaggi turistici nelle citta' del Veneto.</h2></div>
+  <div><h2>Offriamo un servizio pieno di emozioni con viaggi turistici nelle citt&#192; del Veneto.</h2></div>
   <div><p class="testo">
-    VenetoTour si prepone di offrire un servizio interattivo con la possibilita' da parte degli utenti di creare e proporre tour turistici nella Regione Veneto.
-    
-    Ogni settimana nuovi tour disponbili in tutto il Veneto: Padova, Verona, Vicenza, Venezia, Treviso, Belluno, Rovigo.
-    Scegli la citta' di tuo interesse, scegli fra i numerosi tuor disponibili per ogni citta', il percorso piu' interessante.
-    con un semplice click ti iscrivi e sei pronto a partire all'avventura.
-    Puoi partire in compagnia con i tuoi amici, oppure da solo e conoscerne di nuovi.
-    Citta' e paesaggi mozzafiato che ti permettono di scoprire le bellezze del Veneto senza lo stress di lunghi viaggi e ore nel traffico.
-    Non trovi il tour che vorresti fare, organizza il tuo Tour!
-    Permettiamo a ogni nostro utente di organizzare il proprio tour personalizzato per le sue esigenze.
+    VenetoTour si prepone di offrire un servizio interattivo con la possibilit&#192; da parte degli utenti di creare e proporre tour turistici nella Regione Veneto.<br>
+    Ogni settimana nuovi tour disponbili in tutto il Veneto: Padova, Verona, Vicenza, Venezia, Treviso, Belluno, Rovigo.<br>
+    Scegli la citt&#192; di tuo interesse, scegli fra i numerosi tuor disponibili per ogni citt&#192;, il percorso piu' interessante.<br>
+    con un semplice click ti iscrivi e sei pronto a partire all'avventura.<br>
+    Puoi partire in compagnia con i tuoi amici, oppure da solo e conoscerne di nuovi.<br>
+    Citt&#192; e paesaggi mozzafiato che ti permettono di scoprire le bellezze del Veneto senza lo stress di lunghi viaggi e ore nel traffico.<br>
+    Non trovi il tour che vorresti fare, organizza il tuo Tour!<br>
+    Permettiamo a ogni nostro utente di organizzare il proprio tour personalizzato per le sue esigenze.<br>
     
     Parti subito con noi.</p>
   </div>
@@ -35,12 +34,3 @@
 
 <?php include('footer.php') ?>
 
-
-
-
-Il Veneto è una regione del nord-est dell'Italia che si estende dalle Dolomiti al mar Adriatico. Venezia, il capoluogo, è famosa per i canali, l'architettura gotica e il carnevale. Per più di un millennio, tra i secoli VII e XVIII, il Veneto fece parte della potente Repubblica di Venezia. Vicino al Lago di Garda, un lago che bagna le Alpi, sorge Verona, la città medievale dove Shakespeare ambientò "Giulietta e Romeo".
-
-
-VenetotTour si prepone di offrire un servizio interattivo con la possibilità da parte degli utenti di creare e proporre tour turistici nella Regione Veneto e avvalersi della preparazione di una delle guide a noi affiliate.
-
-Che aspetti? Mettiti in marcia.
