300cc19eb2eb729909a1dcb89c7344dd201037a9
vasile <vasiletusa95@yahoo.it>
2019-09-17 10:28:28 +0200

    Update info.php

--- info.php
+++ info.php
@@ -16,14 +16,14 @@
   <div><h1> Veneto Tour ti porta alla scoperta del Veneto</h1></div>
   <div><h2>Offriamo un servizio pieno di emozioni con viaggi turistici nelle citt&agrave; del Veneto.</h2></div>
   <div><p class="testo altezza">
-    VenetoTour si prepone di offrire un servizio interattivo con la possibilit&agrave; da parte degli utenti di creare e proporre tour turistici nella Regione Veneto.<br>
-    Ogni settimana nuovi tour disponbili in tutto il Veneto: Padova, Verona, Vicenza, Venezia, Treviso, Belluno, Rovigo.<br>
-    Scegli la citt&agrave; di tuo interesse, scegli fra i numerosi tour disponibili per ogni citt&agrave;, il percorso più interessante.<br>
-    Con un semplice click ti iscrivi e sei pronto a partire all'avventura.<br>
-    Puoi partire in compagnia con i tuoi amici, oppure da solo e conoscerne di nuovi.<br>
-    Citt&agrave; e paesaggi mozzafiato che ti permettono di scoprire le bellezze del Veneto senza lo stress di lunghi viaggi e ore nel traffico.<br>
-    Non trovi il tour che vorresti fare? Organizza subito il tuo!<br>
-    Permettiamo a ogni nostro utente di organizzare il proprio tour personalizzato per le sue esigenze.<br>
+    VenetoTour si prepone di offrire un servizio interattivo con la possibilit&agrave; da parte degli utenti di creare e proporre tour turistici nella Regione Veneto.
+    Ogni settimana nuovi tour disponbili in tutto il Veneto: Padova, Verona, Vicenza, Venezia, Treviso, Belluno, Rovigo.
+    Scegli la citt&agrave; di tuo interesse, scegli fra i numerosi tour disponibili per ogni citt&agrave;, il percorso più interessante.
+    Con un semplice click ti iscrivi e sei pronto a partire all'avventura.
+    Puoi partire in compagnia con i tuoi amici, oppure da solo e conoscerne di nuovi.
+    Citt&agrave; e paesaggi mozzafiato che ti permettono di scoprire le bellezze del Veneto senza lo stress di lunghi viaggi e ore nel traffico.
+    Non trovi il tour che vorresti fare? Organizza subito il tuo!
+    Permettiamo a ogni nostro utente di organizzare il proprio tour personalizzato per le sue esigenze.
     Unisciti a noi!</p>
   </div>
 </div>
