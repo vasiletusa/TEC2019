252e38ac65927075cd8c7d880e14c6728ca32444
ila <ilarya9531@gmail.com>
2019-09-17 11:40:57 +0200

    boh

--- functions.php
+++ functions.php
@@ -17,9 +17,8 @@ echo"
     <meta name=\"author\" content=\"Rizzo Ilaria, Romito Sara, Vasile Tusa, Salviato Alberto\" />
     <meta name=\"viewport\" content=\"width=device-width, initial-scale = 1.0\" />
 
-	<link rel=\"stylesheet\" href=\"css/style.css\"/ >
+	<link rel=\"stylesheet\"  href=\"css/stylemobile.css\"/ >
 
-    <link rel=\"stylesheet\" href=\"css/stylemobile.css\"  media=\"only screen and (min-width: 600px), only screen and (min-device-width: 600px)\"/>
 
 	<link rel=\"icon\" 
       type=\"image/png\" 
