c7ac29dd6076fe9529d1a4fde7c6a464dbba3827
Your Name <you@example.com>
2019-12-27 18:29:26 +0100

    modifiche css

--- login.php
+++ login.php
@@ -13,52 +13,29 @@
 <?php getBreadcumbs("Login");?>
 
 
-<div class="pageLogin">
+
+<div class=" box" >
 	<?php getMessage();?>
-	<div class="boxLeft" >
+	
+<p id="login" class="coloreAP">Login</p>
+<form action="login.php" method="post" name="login">
+	
 		
-		<p> Iscrizione come privato per partecipare agli eventi</p>
-		<p id="login" class="coloreAP">Log In</p>
-	<form action="login.php" method="post" name="login">
-		
-			
 
-			<div class="input">
-								<label for="Username" class="label username">Username</label>
-								<input id="Username" type="text" name="username" class="input username" placeholder="Inserisci username">
-								<p class="error"><?php getUsernameError($errors); ?></p>
-			</div>
-		<div class="input password">
-								<label for="Password" class="label password"> Password</label>
-								<input id="Password" type="password" name="password" class="input password" placeholder="********">
-								
-		</div>
-							<button type="submit" class="button" name="Login">Login</button>
-	</form>
-	<p id="notRegistered">Non ancora registrato?  <a href='registrazione_utente.php'>Registrati qui</a></p>
+	<div class="input">
+						<label for="Username" class="label">Username</label>
+						<input  type="text" name="username" class="dati" placeholder="Inserisci username">
+						<p class="error"><?php getUsernameError($errors); ?></p>
 	</div>
-
-	<div class="boxRight" >
-<p> Iscrizione come azienda per proporre eventi</p>		
-		<p id="login" class="coloreAP">Log In</p>
-	<form action="login.php" method="post" name="login">
-		
-			
-
-			<div class="input">
-								<label for="Username" class="label username">Username</label>
-								<input id="Username" type="text" name="username" class="input username" placeholder="Inserisci username">
-								<p class="error"><?php getUsernameError($errors); ?></p>
-			</div>
-		<div class="input password">
-								<label for="Password" class="label password"> Password</label>
-								<input id="Password" type="password" name="password" class="input password" placeholder="********">
-								
-		</div>
-							<button type="submit" class="button" name="Login">Login</button>
-	</form>
-	<p id="notRegistered">Non ancora registrato?  <a href='registrazione_utente.php'>Registrati qui</a></p>
+	<div class="input password">
+							<label for="Password" class="label"> Password</label>
+							<input type="password" name="password" class="dati password" placeholder="********">
+							
 	</div>
+	<button type="submit" class="button" name="Login">Login</button>
+
+</form>
+<p id="notRegistered">Non ancora registrato?  <a href='registrazione_utente.php'>Registrati qui</a></p>
 </div>
 
 <?php getfooter()?>
