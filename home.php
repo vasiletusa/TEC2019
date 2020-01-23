47c72a9b17de3436df8022a13d86cd85e3bdcb40
alberto <albertosalviato@gmail.com>
2020-01-23 18:03:01 +0100

    mod w3c

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
@@ -125,7 +126,7 @@
 	</div>
 </div>
 
-<script>
+<script type="text/javascript">
     function myFunction() {
       var x = document.getElementById("myLinks");
       if (x.style.display === "block") {
