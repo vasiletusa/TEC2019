ed93ff895da34c27885fe96ffba196f24f61b717
alberto <albertosalviato@gmail.com>
2020-01-26 18:39:22 +0100

    res

--- footer.php
+++ footer.php
@@ -4,10 +4,6 @@
 		</div>
 		<div> email: venetontour@gmail.com </div>
 		<div> tel: +39 04145678910 </div>
-		<div> <p>
-    	<a href="http://validator.w3.org/check?uri=referer"><img
-      	src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
-  		</p></div>
 		<div class="back-up">	
 		<a href="#" tabindex="1" accesskey="s">TORNA SU</a>
 		</div>
