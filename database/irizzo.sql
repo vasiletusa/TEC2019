56460c44fa4500f82b693530263036f949cf95f0
ila <ilarya9531@gmail.com>
2020-01-27 18:14:49 +0100

    tolti file inutili

--- database/irizzo.sql
+++ database/irizzo.sql
@@ -41,9 +41,14 @@ CREATE TABLE `aziende` (
 --
 
 INSERT INTO `aziende` (`Nome`, `NomeReferente`, `EmailReferente`, `Password`, `Username`) VALUES
-('e', 'b', 'c', ' d', ' e'),
-('bb', 'bb', 'bb@bb', 'bbbb', 'bb'),
-('hh', 'hh', 'hh@hh', 'hhhh', 'hhhh');
+('vivipadova', 'davide', 'davide@vivipadova.it', ' vivipadova', 'vivipadova'),
+('blended', 'matteo', 'matteo@blended.it', 'blended', 'blended'),
+('amici', 'laura', 'laura@amici.it', 'amici', 'amici'),
+('HotelDanieli', 'danieli', 'danieli@hoteldanieli.it', 'danieli', 'danieli'),
+('Garzanti', 'mario', 'mario@garzanti.it', 'garzanti', 'garzanti'),
+('MotorShow', 'james', 'james@motorshow.it', 'motorshow', 'motorshow'),
+('Sweet Elderly People', 'orietta', 'orietta@sweetelderlypeople.it', 'sweetelderlypeople', 'sweetelderlypeople'),
+('iFitness', 'mark', 'mark@ifitness.it', 'ifitness', 'ifitness');
 
 -- --------------------------------------------------------
 
@@ -67,23 +72,17 @@ CREATE TABLE `eventi` (
 --
 
 INSERT INTO `eventi` (`ID`, `Titolo`, `Descrizione`, `Luogo`, `Citta`, `Data`, `Categoria`, `Azienda`) VALUES
-(1, 'aaa', 'aaaaa', 'Padova', 'Venezia', '0000-00-00', 'bbb', 'ff'),
-(2, 'dvdvd', 'dvdvd', 'Venezia', 'Venezia', '0000-00-00', 'vdvdv', 'vdvdv'),
-(3, 'dvdvd', 'dvdvd', 'Belluno', 'Venezia', '0000-00-00', 'vdvdv', 'vdvdv'),
-(4, 'dd', 'dd', 'Verona', 'Venezia', '0000-00-00', 'dd', 'dd\r\n'),
-(5, 'dd', 'dd', 'Treviso', 'Venezia', '0000-00-00', 'dd', 'dd\r\n'),
-(6, 'ss', 'sssss', 'Treviso', 'Venezia', '0000-00-00', 'Famiglie', 'Array'),
-(7, 'hh', 'hhhh', 'Treviso', 'Venezia', '0000-00-00', 'hhhh', 'hhhh'),
-(8, 'gg', 'gg', 'Padova', 'Venezia', '0000-00-00', 'gg', 'hhhh'),
-(9, 'gg', 'gg', 'Verona', 'Venezia', '0000-00-00', 'gg', 'hhhh'),
-(10, 'gg', 'hh', 'Belluno', 'Venezia', '0000-00-00', 'hh', 'hhhh'),
-(11, 'hh', 'hh', 'Rovigo', 'Venezia', '0000-00-00', 'hh', 'hhhh'),
-(12, 'nn', 'nn', 'Rovigo', 'Venezia', '0000-00-00', 'nn', 'hhhh'),
-(13, 'mm', 'mm', 'Rovigo', 'Venezia', '0000-00-00', 'Cultura', 'hhhh'),
-(14, 'mm', 'mm', 'Rovigo', 'Venezia', '0000-00-00', 'mm', 'hhhh'),
-(15, 'mm', 'mm', 'Rovigo', 'Venezia', '0000-00-00', 'mm', 'hhhh'),
-(16, 'mm', 'mm', 'Padova', 'Venezia', '0000-00-00', 'mm', 'hhhh'),
-(17, 'aqaqa', 'aqaq', 'aqaq', 'Belluno', '0000-00-00', 'Discoteca', 'hhhh');
+(1, 'Scopri Giotto', 'Padova è nota per gli affreschi di Giotto della Cappella degli Scrovegni', 'Padova', 'Padova', '2020-02-20', 'Cultura', 'vivipadova'),
+(2, 'Mercoledi universitario', 'il Mercoledi universitario e il giorno preferito dagli studenti di Padova, in quanto dimenticano lo studio e pensano al divertimento.', 'Padova', 'Padova', '2020-03-30', 'Giovani', 'amici'),
+(3, 'Hotel&Conoscenza', 'Danieli Hotel e lieto di presentare "Soggiorni Autore", quattro imperdibili appuntamenti con la storia di grandi personaggi che vi hanno alloggiato.', 'Venezia', 'Venezia', '2020-03-19', 'Cultura', 'HotelDanieli'),
+(4, 'MotorShow', 'Evento espositivo sui nuovi motori elettrici', 'Verona', 'Verona', '2020-03-15', 'Giovani', 'MotorShow'),
+(5, 'Auto&Moto Epoca', 'Evento espositivo su tutte le oldtimers che hanno fatto la storia automobilistica', 'Treviso', 'Treviso', '2020-03-18', 'Spettacolo', 'MotorShow'),
+(6, 'Manutenzione Auto', 'Presentazione Libro Manutenzione della propria auto', 'Treviso', 'Treviso','2020-03-22', 'Famiglie', 'Garzanti'),
+(7, 'Presentazione Il Giorgione', 'Presentazione della biografia del Giorgione, celebre pittore di Castelfranco Veneto', 'Treviso', 'Treviso', '2020-04-01', 'Cultura', 'Garzanti'),
+(8, 'Zumba', 'Presentazione corso di Zumba per persone della terza età', 'Venezia', 'Venezia', '2020-04-02', 'Famiglie', 'iFitness'),
+(9, 'Bingo', 'Serata Bingo offerta dalla casa di riposo Sweet Elderly People', 'Verona', 'Verona', '2020-04-10', 'Famiglie', 'Sweet Elderly People'),
+(10, 'Alfa Romeo motorshow', 'Evento dedicato a tutti gli appassionati del celebre Marchio automobilistico', 'Belluno', 'Belluno', '2020-04-10', 'Spettacolo', 'MotorShow'),
+(11, 'AperyShow', 'Evento benefico per la raccolta fondi contro la SLA', 'Rovigo', 'Rovigo', '2020-04-11', 'Famiglie', 'AperyShow');
 
 -- --------------------------------------------------------
 
@@ -101,10 +100,22 @@ CREATE TABLE `log` (
 -- Dump dei dati per la tabella `log`
 --
 
-INSERT INTO `log` (`Username`, `Password`, `Tipo`) VALUES
-('hhhh', 'hhhh', 'azienda'),
-('User', 'user', 'utente');
 
+INSERT INTO `log` (`Username`, `Password`, `Tipo`) VALUES
+('vivipadova', 'vivipadova', 'azienda'),
+('blended', 'blended', 'azienda'),
+('amici', 'amici', 'azienda'),
+('danieli', 'danieli', 'azienda'),
+('garzanti', 'garzanti', 'azienda'),
+('motorshow', 'motorshow', 'azienda'),
+('sweetelderlypeople', 'sweetelderlypeople', 'azienda'),
+('ifitness', 'ifitness', 'azienda'),
+('asalviato', 'asalviato', 'user'),
+('irizzo', 'irizzo', 'user'),
+('sromito', 'sromito', 'user'),
+('vtusa', 'vtusa', 'user'),
+('admin', 'admin', 'admin'),
+('user', 'user', 'user');
 -- --------------------------------------------------------
 
 --
@@ -157,21 +168,20 @@ CREATE TABLE `utenti` (
   `Cognome` varchar(50) NOT NULL,
   `Username` varchar(50) NOT NULL,
   `Password` varchar(10) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Ruolo` varchar(255) NOT NULL DEFAULT 'User'
+  `Email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `utenti`
 --
 
-INSERT INTO `utenti` (`Nome`, `Cognome`, `Username`, `Password`, `Email`, `Ruolo`) VALUES
-('Admin', 'Admin', 'Admin', 'admin', 'admin@admin', 'Admin'),
-('Alberto', 'Salviato', 'ASalviato', 'ASalviat', 'alberto.salviato@studenti.unipd.it', 'User'),
-('ilaria', 'Rizzo', 'IRizzo', 'IRizzo', 'ilaria.rizzo.5@studenti.unipd.it', 'User'),
-('Sara', 'Romito', 'SRomito', 'Sromito', 'sara.romito@studenti.unipd.it', 'User'),
-('User', 'User', 'User', 'user', 'user@user', 'User'),
-('Vasile', 'Tusa', 'VTusa', 'VTusa', 'vasile.tusa@studenti.unipd.it', 'User');
+INSERT INTO `utenti` (`Nome`, `Cognome`, `Username`, `Password`, `Email`) VALUES
+('Admin', 'Admin', 'admin', 'admin', 'admin@admin'),
+('Alberto', 'Salviato', 'asalviato', 'asalviato', 'alberto.salviato@studenti.unipd.it'),
+('ilaria', 'Rizzo', 'irizzo', 'irizzo', 'ilaria.rizzo.5@studenti.unipd.it'),
+('Sara', 'Romito', 'sromito', 'sromito', 'sara.romito@studenti.unipd.it',),
+('User', 'User', 'user', 'user', 'user@user',),
+('Vasile', 'Tusa', 'vtusa', 'vtusa', 'vasile.tusa@studenti.unipd.it');
 
 --
 -- Indici per le tabelle scaricate
